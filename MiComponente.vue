2124b1c9c7f8be14de9a0f0decafd4d447a83892
NEBULA1978 <rpascualey@gmail.com>
2023-09-03 00:44:00 +0200

    creando un componente para las rutas

---
+++ MiComponente.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- Aquí colocas el código HTML del componente -->
+    <div>
+        <h1>Mi Componente</h1>
+        <p>Este es un componente de ejemplo.</p>
+    </div>
+    <script src="https://unpkg.com/vue@3"></script>
+    <script src="https://unpkg.com/vue-router@4"></script>
+</template>
+
+<script>
+
+// Aquí colocas el código JavaScript del componente
+export default {
+    name: 'MiComponente', // Nombre del componente
+}
+
+</script>
+
+<style scoped>
+/* Aquí colocas el CSS del componente */
+h1 {
+    color: blue;
+}
+</style>
